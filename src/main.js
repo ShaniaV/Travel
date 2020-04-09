3ae4ab60d0a6b61ec7d5e0356e284e7a2dbd8f3b
ShaniaV <Shania_V@outlook.com>
2020-04-09 18:45:36 +0800

    'travel'

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 解决 移动端浏览器点击事件延迟200毫秒执行
+// npm install fastclick --save
+import fastClick from 'fastclick'
+// 重置css样式  统一不同手机不同浏览器默认样式不一致
+import './assets/styles/reset.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // router: router,  ES6语法，键和值相同时可以只写一个
   router,
+  // components: { App: App },
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+// @代表src目录
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      // name: 'HelloWorld',
+      name: 'Home',
+      // component: HelloWorld
+      component: Home
     }
   ]
 })
